8be800fb65dd6671d98bcd837ba1f80adbb644b4
Botir <tiboruz@gmail.com>
2025-09-18 16:25:58 +0500

    dfasdfasdfasdfsdfgsdga

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -63,9 +63,55 @@ class ItemController extends Controller
         //
     }
     public function items(Request $request){
+        $bToken = $request->header('Authorization');
+        
+        
+        $token=substr($bToken,7);
+        if ($token!=="xF38j92x81Sdf93Jskd82HsPzks82ks9aP9a"){
+            return [
+                'code'=>500,
+                'status'=>'error',
+                'message'=>"Wrong Token",
+                
+            ];
+        }
+
+        $reqBody=$request->all();
+        $page=$reqBody['page'] ?? 0;
+        $size = isset($reqBody['size']) 
+        ? min((int) $reqBody['size'], 500) 
+        : 500;
+
+        $count=Item::count();
+        if($count==0){
+            return [
+                'code'=>400,
+                'status'=>'error',
+                'message'=>"no Items Found",
+                
+            ];
+        }
+        $total=(int) ceil($count/$size);
+
+        if($page>$total){
+            return [
+                'code'=>500,
+                'status'=>'error',
+                'message'=>"page number out of range",
+                
+            ];
+        }
         return [
+            'code'=>200,
             'status'=>'ok',
-            'data'=>$request->all()
+            
+            'data'=>[
+                
+            ],
+            'page'=>$page,
+            'size'=>$size,
+            'record_count'=>$count,
+            'total_pages'=>$total
         ];
     }
 }
