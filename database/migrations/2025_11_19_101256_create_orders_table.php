2f9f95ab31052df96437363b842c4767c182b5ec
Botir <tiboruz@gmail.com>
2025-11-25 16:28:45 +0500

    sdfasfawerferf

--- database/migrations/2025_11_19_101256_create_orders_table.php
+++ database/migrations/2025_11_19_101256_create_orders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->uuid()->primary();
+            $table->uuid('id')->primary();
             $table->integer('type')->default(1);//1Andalusgo,2Wolt
             $table->string('code')->nullable();
             $table->string("client")->nullable();
