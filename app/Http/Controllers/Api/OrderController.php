62d4033c3ea48835974ace9c4dd3d5d9f02aebcf
Botir <tiboruz@gmail.com>
2025-12-02 16:08:06 +0500

    sdfawefwegqergaeg

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\OrderStatus;
 
 class OrderController extends Controller
 {
@@ -28,9 +31,31 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
+        $data=$request->all();
+        $order=Order::create(
+            [
+                "code"=>$data["id"],
+
+            ]
+        );
+        $items=$data["items"];
+        foreach($items as $item){
+            $orderItem=OrderItem::create([
+                "order_id"=>$order->id,
+                "item_id"=>$item["productId"],
+                "order_qty"=>$item["price"],
+                "order_price"=>$item["quantity"]
+            ]);
+
+        }
+        OrderStatus::create([
+            "order_id"=>$order->id,
+            "status"=>"new"
+        ]);
+
         return [
-            'id'=>'asdasdasdasdad',
-            'status'=>'new'
+            "id"=>$order->id,
+            "status"=>"new"
         ];
        
     }
