cc0cfa7eaac2686033b9cebbb6958faedf917eff
Botir <tiboruz@gmail.com>
2025-12-10 10:17:25 +0500

    sdfsadfsdfasdf

---
+++ app/Console/Commands/Wolt.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Http\Controllers\Api\WoltController;
+
+class Wolt extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'wolt';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $wolt= new WoltController;
+        $token=$wolt->woltTokenRefresh();
+            $this->info($token);
+    }
+}
