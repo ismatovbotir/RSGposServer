0daa50d78488d0721bda9ed32629d0cf266843f9
Botir <tiboruz@gmail.com>
2025-12-11 15:57:35 +0500

    sdfdfsdfsaf rer grg e

--- resources/views/livewire/wolt/token.blade.php
+++ resources/views/livewire/wolt/token.blade.php
@@ -29,11 +29,11 @@
     <div class="col-lg-3 col-sm-6">
         <div class="form-group">
             <div class="form-control-wrap">
-                @if($isRefresh)
-                    <a href="#" class="btn btn-success" wire:click.prevent="refreshToken">Refresh Token</a>
-                @else
-                    <button class="btn btn-info">Token</button>
-                @endif
+              
+                <button class="btn btn-success" wire:click="refreshToken">Refresh Token</button>
+               
+                <button class="btn btn-info">Token</button>
+                
             </div>
         </div>
     </div>
