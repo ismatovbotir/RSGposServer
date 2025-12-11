6ee8c52efae750921d96a937d61c2afbf47eff25
Botir <tiboruz@gmail.com>
2025-12-11 16:27:23 +0500

    sdfsfsdf

--- resources/views/livewire/wolt/token.blade.php
+++ resources/views/livewire/wolt/token.blade.php
@@ -37,7 +37,9 @@
             </div>
         </div>
     </div>
-
+    @php
+    dd($wolt_token)
+    @endphp
     <div class="col-lg-12 col-sm-12">
         <div class="form-group">
             <div class="form-control-wrap">
