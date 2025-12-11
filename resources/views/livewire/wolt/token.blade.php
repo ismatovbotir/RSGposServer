86b169b7ab485086955a96f099bc6201e5291410
Botir <tiboruz@gmail.com>
2025-12-11 11:43:44 +0500

    sdafsdafasdfagbdfag adfg da

--- resources/views/livewire/wolt/token.blade.php
+++ resources/views/livewire/wolt/token.blade.php
@@ -29,8 +29,11 @@
     <div class="col-lg-3 col-sm-6">
         <div class="form-group">
             <div class="form-control-wrap">
-                <button class="btn btn-info">Token</button>
+                @if($refresh)
                 <button class="btn btn-success">Refresh Token</button>
+                @else
+                <button class="btn btn-info">Token</button>
+                @endif
             </div>
         </div>
     </div>
