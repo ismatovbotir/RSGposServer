d98ba9fb6c7f9a105afa526fe89b0bcaf7352598
Botir <tiboruz@gmail.com>
2026-01-16 14:52:23 +0500

    sdfasdfewrferfer

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -38,7 +38,7 @@ class OrderController extends Controller
     public function show(string $id)
     {
         $order=Order::where('id',$id)->with(['items.item','lastStatus'])->first();
-        dd($order);
+        return view('order.show',['order'=>$order]);
     }
 
     /**
