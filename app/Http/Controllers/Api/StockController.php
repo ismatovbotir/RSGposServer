e4eb61ccf86769fbf252c1802d1f1f796f2378b5
Botir <tiboruz@gmail.com>
2026-02-15 14:07:39 +0500

    sdfsadfaffergaaergerg

--- app/Http/Controllers/Api/StockController.php
+++ app/Http/Controllers/Api/StockController.php
@@ -47,12 +47,12 @@ class StockController extends Controller
      */
     public function show(string $id)
     {
-        return response()->json(['status' => 'done','data'=>$id]);
-        Stock::where('stock_date','<>',$id)->update([
+        //return response()->json(['status' => 'done','data'=>$id]);
+        $res=Stock::where('stock_date','<>',$id)->update([
             'qty'=>0,
             'cost'=>0
         ]);
-        return response()->json(['status' => 'done']);
+        return response()->json(['status' => $res]);
     }
 
     /**
