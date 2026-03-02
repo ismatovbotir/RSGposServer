ae71f87ceb06bf4151ce14727302139942df45af
Botir <tiboruz@gmail.com>
2026-03-02 10:43:48 +0500

    fsdsadfasd fsad fsdaf sda fsdfffff

--- resources/views/admin/receipt/index.blade.php
+++ resources/views/admin/receipt/index.blade.php
@@ -2,89 +2,90 @@
 
 @section('content')
 
-    
+
 <div class="nk-content ">
-                    <div class="container-fluid">
-                        <div class="nk-content-inner">
-                            <div class="nk-content-body">
-                                <div class="nk-block-head nk-block-head-sm">
-                                    <div class="nk-block-between">
-                                        <div class="nk-block-head-content">
-                                            <h3 class="nk-block-title page-title">Receipts</h3>
-                                            <div class="nk-block-des text-soft">
-                                                <p>You have total {{$receipts->count()}} receipts.</p>
-                                            </div>
-                                        </div><!-- .nk-block-head-content -->
-                                        <div class="nk-block-head-content">
-                                            
-                                        </div><!-- .nk-block-head-content -->
-                                    </div><!-- .nk-block-between -->
-                                </div><!-- .nk-block-head -->
-                                <div class="nk-block">
-                                    <div class="row g-gs">
-                                    @foreach($receipts as $receipt)       
-                                    <div class="col-sm-6 col-lg-4 col-xxl-3">
-                                         
-                                        <div class="card card-bordered h-100">
-                                                <div class="card-inner">
-                                                    <div class="project">
-                                                        <div class="project-head">
-                                                            <a href="{{route('admin.receipt.show',['receipt'=>$receipt->id])}}" class="project-title">
-                                                                <div class="user-avatar sq bg-purple"><span>DD</span></div>
-                                                                <div class="project-info">
-                                                                    <h6 class="title">Shop {{$receipt->shop_id}}, Kassa-{{$receipt->pos_id}}</h6>
-                                                                    <span class="sub-text">Kassir {{$receipt->cashier}}</span>
-                                                                </div>
-                                                            </a>
-                                                            <div class="drodown">
-                                                                <a href="#" class="dropdown-toggle btn btn-sm btn-icon btn-trigger mt-n1 mr-n1" data-toggle="dropdown"><em class="icon ni ni-more-h"></em></a>
-                                                                <div class="dropdown-menu dropdown-menu-right">
-                                                                    <ul class="link-list-opt no-bdr">
-                                                                        <li><a href="html/apps-kanban.html"><em class="icon ni ni-eye"></em><span>View Project</span></a></li>
-                                                                        <li><a href="#"><em class="icon ni ni-edit"></em><span>Edit Project</span></a></li>
-                                                                        <li><a href="#"><em class="icon ni ni-check-round-cut"></em><span>Mark As Done</span></a></li>
-                                                                    </ul>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="project-details">
-                                                            <p>Design and develop the DashLite template for Envato Marketplace.</p>
-                                                        </div>
-                                                        <div class="project-progress">
-                                                            <div class="project-progress-details">
-                                                                <div class="project-progress-task"><em class="icon ni ni-check-round-cut"></em><span>3 Tasks</span></div>
-                                                                <div class="project-progress-percent">93.5%</div>
-                                                            </div>
-                                                            <div class="progress progress-pill progress-md bg-light">
-                                                                <div class="progress-bar" data-progress="93.5" style="width: 93.5%;"></div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="project-meta">
-                                                            <ul class="project-users g-1">
-                                                                <li>
-                                                                    <div class="user-avatar sm bg-primary"><span>A</span></div>
-                                                                </li>
-                                                                <li>
-                                                                    <div class="user-avatar sm bg-blue"><img src="./images/avatar/b-sm.jpg" alt=""></div>
-                                                                </li>
-                                                                <li>
-                                                                    <div class="user-avatar bg-light sm"><span>+12</span></div>
-                                                                </li>
-                                                            </ul>
-                                                            <span class="badge badge-dim badge-warning"><em class="icon ni ni-clock"></em><span>5 Days Left</span></span>
-                                                        </div>
-                                                    </div>
+    <div class="container-fluid">
+        <div class="nk-content-inner">
+            <div class="nk-content-body">
+                <div class="nk-block-head nk-block-head-sm">
+                    <div class="nk-block-between">
+                        <div class="nk-block-head-content">
+                            <h3 class="nk-block-title page-title">Receipts</h3>
+                            <div class="nk-block-des text-soft">
+                                <p>You have total {{$receipts->count()}} receipts.</p>
+                            </div>
+                        </div><!-- .nk-block-head-content -->
+                        <div class="nk-block-head-content">
+
+                        </div><!-- .nk-block-head-content -->
+                    </div><!-- .nk-block-between -->
+                </div><!-- .nk-block-head -->
+                <div class="nk-block">
+                    <div class="row g-gs">
+                        @foreach($receipts as $receipt)
+                        <div class="col-sm-6 col-lg-4 col-xxl-3">
+
+                            <div class="card card-bordered h-100">
+                                <div class="card-inner">
+                                    <div class="project">
+                                        <div class="project-head">
+                                            <a href="{{route('admin.receipt.show',['receipt'=>$receipt->id])}}" class="project-title">
+                                                <div class="user-avatar sq bg-purple"><span>DD</span></div>
+                                                <div class="project-info">
+                                                    <h6 class="title">Shop {{$receipt->shop_id}}, Kassa-{{$receipt->pos_id}}</h6>
+                                                    <span class="sub-text">Kassir {{$receipt->cashier}}</span>
+                                                </div>
+                                            </a>
+                                            <div class="drodown">
+                                                <a href="#" class="dropdown-toggle btn btn-sm btn-icon btn-trigger mt-n1 mr-n1" data-toggle="dropdown"><em class="icon ni ni-more-h"></em></a>
+                                                <div class="dropdown-menu dropdown-menu-right">
+                                                    <ul class="link-list-opt no-bdr">
+                                                        <li><a href="html/apps-kanban.html"><em class="icon ni ni-eye"></em><span>View Project</span></a></li>
+                                                        <li><a href="#"><em class="icon ni ni-edit"></em><span>Edit Project</span></a></li>
+                                                        <li><a href="#"><em class="icon ni ni-check-round-cut"></em><span>Mark As Done</span></a></li>
+                                                    </ul>
                                                 </div>
                                             </div>
                                         </div>
-                                  
-                                        
+                                        <div class="project-details">
+                                            <p>Design and develop the DashLite template for Envato Marketplace.</p>
+                                        </div>
+                                        <div class="project-progress">
+                                            <div class="project-progress-details">
+                                                <div class="project-progress-task"><em class="icon ni ni-check-round-cut"></em><span>3 Tasks</span></div>
+                                                <div class="project-progress-percent">93.5%</div>
+                                            </div>
+                                            <div class="progress progress-pill progress-md bg-light">
+                                                <div class="progress-bar" data-progress="93.5" style="width: 93.5%;"></div>
+                                            </div>
+                                        </div>
+                                        <div class="project-meta">
+                                            <ul class="project-users g-1">
+                                                <li>
+                                                    <div class="user-avatar sm bg-primary"><span>A</span></div>
+                                                </li>
+                                                <li>
+                                                    <div class="user-avatar sm bg-blue"><img src="./images/avatar/b-sm.jpg" alt=""></div>
+                                                </li>
+                                                <li>
+                                                    <div class="user-avatar bg-light sm"><span>+12</span></div>
+                                                </li>
+                                            </ul>
+                                            <span class="badge badge-dim badge-warning"><em class="icon ni ni-clock"></em><span>5 Days Left</span></span>
+                                        </div>
                                     </div>
-                                    @endforeach
-                                </div><!-- .nk-block -->
+                                </div>
                             </div>
                         </div>
+
+
+                    
+                    @endforeach
                     </div>
-                </div>  
+                </div><!-- .nk-block -->
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
